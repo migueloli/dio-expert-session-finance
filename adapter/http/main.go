201bf43020e3ac45707a5319612e8e1967b820c4
Miguel Oliveira <desenv.inovar.sistemas@gmail.com>
2020-08-24 11:31:28 -0300

    - Add prometheus. - Created endpoint for prometheus metrics.

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/migueloli/dio-expert-session-finance.git/adapter/http/actuator"
 	"github.com/migueloli/dio-expert-session-finance.git/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // Init is used to create the endpoints and start the server.
 func Init() {
 	http.HandleFunc("/health", actuator.Health)
+	http.Handle("/metrics", promhttp.Handler())
 
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateTransaction)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/migueloli/dio-expert-session-finance.git
 go 1.15
 
 require (
+	github.com/prometheus/client_golang v1.7.1
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
 	golang.org/x/tools v0.0.0-20200823205832-c024452afbcd // indirect
 )
